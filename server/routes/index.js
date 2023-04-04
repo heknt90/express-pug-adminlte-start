72f718d27ce225b3263841727cb209a816ef6742
Nikita Efremov <heknt90@mail.ru>
2023-04-04 16:38:08 +0300

    newGood page added at index routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,26 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const axios = require('axios') 
+const { getGoodService } = require('../services/goodServices');
 
-const baseUrl = "http://localhost:3000"
-
-/* GET home page. */
+/* Home page. */
 router.get('/', function(req, res) {
   res.render('index', {
     title: "Goods list"
   });
 });
 
-/* GET home page. */
+/* New good page */
+router.get('/good/new', function(req, res) {
+  
+  res.render('newgood', {
+    title: "New good creation form"
+  })
+}) 
+
+/* Good view page */
 router.get('/good/:goodId', function(req, res) {
   const { goodId } = req.params
-  axios.get(`${baseUrl}/api/goods/${goodId}`)
-    .then(response => {
-      res.render('good', {
-        title: response.data.data.name + " | Good",
-        good: response.data.data
+  getGoodService(goodId)
+    .then(good => {
+      res.render("good", {
+        title: good.name + " | Good",
+        good
+      })
     })
-  })
 });
 
 module.exports = router;
