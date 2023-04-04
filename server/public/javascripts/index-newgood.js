0383d79fdd1fa4ec633b5eb093fd281cab578dbe
Nikita Efremov <heknt90@mail.ru>
2023-04-04 18:37:02 +0300

    Clear form after creating new good at /goods/new page TODOS updated

--- server/public/javascripts/index-newgood.js
+++ server/public/javascripts/index-newgood.js
@@ -1,76 +1,85 @@
-const creationNewGoodForm = document.getElementById("new_good_creation_form")
-creationNewGoodForm.dataset.formIndex = 0
+(function() {
+    'use strict'
 
-// Validation config
-const creationNewGoodConfig = {
-    name: ['required'],
-    desc: ['required'],
-    'price-current': ['required'],
-    'price-full': ['required'],
-    thumb1: ['urlValidator'],
-    thumb2: ['urlValidator']
-}
-
-const forms = [
-    {
-        node: creationNewGoodForm,
-        validations: creationNewGoodConfig
+    const creationNewGoodForm = document.getElementById("new_good_creation_form")
+    creationNewGoodForm.dataset.formIndex = 0
+    
+    // Validation config
+    const creationNewGoodConfig = {
+        name: ['required'],
+        desc: ['required'],
+        'price-current': ['required'],
+        'price-full': ['required'],
+        thumb1: ['urlValidator'],
+        thumb2: ['urlValidator']
     }
-]
-
-creationNewGoodForm.addEventListener("submit", async (event) => {
-    event.preventDefault()
-    if (validate(event.target)) {
-        const data = await JSON.stringify(createObjectFormData(event.target))
-        fetch('http://localhost:3000/api/goods/new', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: data
-        })
-            .then(response => console.log(response.json()))
-    }
-})
-
-function validate(form) {
-    if (form.dataset.form === "validate") {
-        const validations = forms[form.dataset.formIndex].validations
-        for (let fieldName of Object.keys(validations)) {
-            const fieldValue = form[fieldName].value
-            for (let checkName of validations[fieldName]) {
-                let result = true
-                switch (checkName) {
-                    case "required":
-                        result = requiredValidation(fieldValue)
-                        break;
-                    case "urlValidator":
-                        result = urlValidation(fieldValue)
-                        break;
-                }
-                if (result === false) {
-                    return false
-                }
-            }      
+    
+    const forms = [
+        {
+            node: creationNewGoodForm,
+            validations: creationNewGoodConfig
         }
-        return true
-    }
-
-    function requiredValidation(value) {
-        return !!value
-    }
-
-    function urlValidation(value) {
-        return true
-    }
-}
-
-function createObjectFormData(form) {
-    const object = {}
-    for (let element of form.elements) {
-        if (!(element.type && element.type === "submit")) {
-            object[element.name] = element.value
+    ]
+    
+    creationNewGoodForm.addEventListener("submit", async (event) => {
+        event.preventDefault()
+        if (validate(event.target)) {
+            const data = await JSON.stringify(createObjectFormData(event.target))
+            await fetch('http://localhost:3000/api/goods/new', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: data
+            })
+            for (let field of event.target.elements) {
+                console.log
+                if (!(field.type && field.type === "submit")) {
+                    field.value = ""
+                }
+            }
+        }
+    })
+    
+    function validate(form) {
+        if (form.dataset.form === "validate") {
+            const validations = forms[form.dataset.formIndex].validations
+            for (let fieldName of Object.keys(validations)) {
+                const fieldValue = form[fieldName].value
+                for (let checkName of validations[fieldName]) {
+                    let result = true
+                    switch (checkName) {
+                        case "required":
+                            result = requiredValidation(fieldValue)
+                            break;
+                        case "urlValidator":
+                            result = urlValidation(fieldValue)
+                            break;
+                    }
+                    if (result === false) {
+                        return false
+                    }
+                }      
+            }
+            return true
+        }
+    
+        function requiredValidation(value) {
+            return !!value
+        }
+    
+        function urlValidation(value) {
+            return true
         }
     }
-    return object
-}
+    
+    function createObjectFormData(form) {
+        const object = {}
+        for (let element of form.elements) {
+            if (!(element.type && element.type === "submit")) {
+                object[element.name] = element.value
+            }
+        }
+        return object
+    }
+})()
